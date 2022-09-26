98d8b6b52a3855656530ec2d22bae2a91d8c7f8b
Thabo Tshabalala <47thabo@gmail.com>
2022-09-26 15:50:05 +0200

    push

--- admin/viewm.php
+++ admin/viewm.php
@@ -98,7 +98,7 @@
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="submit" class="btn btn-success">Add Music Video</button>
+                        <a href="remmes.php?mid=<?php echo $m_id;?>"><button type="submit" class="btn btn-success">Add Music Video</button></a>
                     </div>
                 </form>
             </div>
